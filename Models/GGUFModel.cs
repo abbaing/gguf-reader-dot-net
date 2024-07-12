5e94fa03871cd9a728c8c6db09329e6665543f3a
David Abba <david_abba@outlook.com>
2024-07-12 12:44:52 +0200

    Added GGUF Models

---
+++ Models/GGUFModel.cs
@@ -0,0 +1,8 @@
+﻿namespace GGUFReader.Models;
+
+public struct GGUFModel(ModelType model)
+{
+    public ModelType Model { get; set; } = model;
+
+    public readonly override string ToString() => Model.ToString();
+}
---
+++ Models/ModelType.cs
@@ -0,0 +1,10 @@
+﻿namespace GGUFReader.Models;
+
+public enum ModelType
+{
+    LLAMA_2_7B,
+    MISTRAL_7B,
+    MIXTRAL_7B,
+    OPENHERMES_7B,
+    CHAT_GPT
+}
