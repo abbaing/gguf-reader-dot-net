706a2ca67563a1631c4506bdf9ccb3257a076f21
David Abba <david_abba@outlook.com>
2024-07-19 20:25:27 +0200

    Added dependency injection

---
+++ GGUFReader/Utils/DIManager.cs
@@ -0,0 +1,19 @@
+﻿using GGUFReader.Services;
+using LLama.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.ML;
+
+namespace GGUFReader.Utils;
+
+public static class DIManager
+{
+    public static IServiceCollection AddMiLibreria(this IServiceCollection services)
+    {
+        services.AddSingleton<MLContext>();
+
+        services.AddScoped<IInferenceParamsService, InferenceParamsService>();
+        services.AddScoped<ILlamaModelService, LlamaModelService>();
+
+        return services;
+    }
+}
